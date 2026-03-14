a467eaf70266c4f6ae5061db08c7e209eee42fe3
alexiastfn <stefanalexia01@gmail.com>
2026-03-14 23:32:07 +0200

    companion objects

--- 04/kotlin/generics/src/main/kotlin/Main.kt
+++ 04/kotlin/generics/src/main/kotlin/Main.kt
@@ -14,6 +14,10 @@ fun main() {
     var total = StudentProgress.total
     println("$answered question answered from $total")
 
+    println("----------------------")
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+    println("${Quiz.StudentProgress2.answered} of ${Quiz.StudentProgress2.total} answered.")
+
 
 
 }
---
+++ 04/kotlin/generics/src/main/kotlin/Quiz.kt
@@ -0,0 +1,16 @@
+package org.example
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+
+    // Companion Objects belong to class, not to the instances of Quiz class
+    // kind of static members from java
+    companion object StudentProgress2 {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+
+}
