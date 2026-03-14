cc1c72fedc7ad4128afa96b13c2d3fc7afcc559b
alexiastfn <stefanalexia01@gmail.com>
2026-03-14 23:16:31 +0200

    enum class added

---
+++ 04/kotlin/generics/src/main/kotlin/Difficulty.kt
@@ -0,0 +1,7 @@
+package org.example
+
+// An enum class is used to create types with a limited set of possible values
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
--- 04/kotlin/generics/src/main/kotlin/Question.kt
+++ 04/kotlin/generics/src/main/kotlin/Question.kt
@@ -22,12 +22,12 @@ package org.example
 // Generic Types allow you to have a single property that can have differing data types
 
 
-class Question<T> (val questionText: String, val answer: T, val difficulty: String) {
+class Question<T> (val questionText: String, val answer: T, val difficulty: Difficulty) {
 
 }
 
 fun questions() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
