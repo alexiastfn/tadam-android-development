cad4ae593b38ce9559306876ecc8158391e17a4a
alexiastfn <stefanalexia01@gmail.com>
2026-03-14 23:11:38 +0200

    generic question

---
+++ 04/kotlin/generics/src/main/kotlin/Question.kt
@@ -0,0 +1,33 @@
+package org.example
+
+//class FillInTheBlankQuestion(
+//    val questionText: String,
+//    val answer: String,
+//    val difficulty: String
+//)
+
+//class NumericQuestion(
+//    val questionText: String,
+//    val answer: Int,
+//    val difficulty: String
+//)
+
+//class NumericQuestion(
+//    val questionText: String,
+//    val answer: Int,
+//    val difficulty: String
+//)
+
+// When you want a property to have differing data types, subclassing is not the answer
+// Generic Types allow you to have a single property that can have differing data types
+
+
+class Question<T> (val questionText: String, val answer: T, val difficulty: String) {
+
+}
+
+fun questions() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}
