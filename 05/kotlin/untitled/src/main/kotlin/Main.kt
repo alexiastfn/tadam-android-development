b37126765c8fee5867ad586413552ca77fcbf663
alexiastfn <stefanalexia01@gmail.com>
2026-03-26 19:47:44 +0200

    for each in kotlin

--- 05/kotlin/untitled/src/main/kotlin/Main.kt
+++ 05/kotlin/untitled/src/main/kotlin/Main.kt
@@ -6,26 +6,30 @@ import org.example.collections.mapBasic
 import org.example.collections.modifyPlanets
 import org.example.collections.planets
 import org.example.collections.setBasic
+import org.example.functional.forEachBasic
 
 
 fun main() {
 
+//    println("------------")
+//    planets()
+//    modifyPlanets(planets())
+//
+//
+//    println("------------")
+//    listsBasic()
+//
+//    println("------------")
+//    listsBasic2()
+//
+//    println("------------")
+//    setBasic()
+//
+//
+//    println("------------")
+//    mapBasic()
+
     println("------------")
-    planets()
-    modifyPlanets(planets())
-
-
-    println("------------")
-    listsBasic()
-
-    println("------------")
-    listsBasic2()
-
-    println("------------")
-    setBasic()
-
-
-    println("------------")
-    mapBasic()
+    forEachBasic()
 
 }
